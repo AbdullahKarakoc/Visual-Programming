4fb1ef6b60aa79874061d33ce90a8268bdd0136c
AbdullahKarakoc <abdullahkrkc1453@gmail.com>
2024-09-28 01:02:29 +0300

    Add project files.

---
+++ Week2/Week2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2
+{
+
+    class Bread
+    {
+        public int counter;
+    }
+
+    static class Butter
+    {
+        public static int counter;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //const double pi = 3.14;
+            //double r;
+
+            //console.writeline("enter radius");
+            //r = convert.todouble(console.readline());
+
+            //double areacircle = pi * r * r;
+            //console.writeline("radius: {0}, area: {1}", r, areacircle);
+            //console.readline();
+
+
+
+
+            //Bread a = new Bread();
+            //a.counter = 3;
+            //Console.WriteLine("Bread: " + a.counter);
+
+            //Butter.counter = 7;
+            //Console.WriteLine("Butter: " + Butter.counter);
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
