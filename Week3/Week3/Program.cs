484d1f0a6e0079b6bfe0fbdf34d9d71dea650e3f
AbdullahKarakoc <abdullahkrkc1453@gmail.com>
2024-10-02 18:13:48 +0300

    feat: add week3 file with class structure

---
+++ Week3/Week3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3
+{
+    internal class Program
+    {
+
+        class MyArray
+        {
+            double getAverage(int[] arr, int size)
+            {
+                int i;
+                double avg;
+                int sum = 0;
+
+                for (i = 0; i < size; ++i)
+                {
+                    sum += arr[i];
+                }
+                avg = (double)sum / size;
+                return avg;
+            }
+
+
+            static void Main(string[] args)
+            {
+                MyArray app = new MyArray();
+
+                int[] balance = new int[] { 1000, 2, 3, 17, 50 };
+                double avg;
+
+                avg = app.getAverage(balance, 5);
+
+                Console.WriteLine("Average value is: {0} ", avg);
+                Console.ReadLine();
+            }
+        }
+    }
+}
