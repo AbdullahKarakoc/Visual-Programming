14c6f2fc78341c34f04f3d400b0611c856543536
AbdullahKarakoc <abdullahkrkc1453@gmail.com>
2024-10-09 19:03:57 +0300

    feat: add access modifier and getter setter method at week4

---
+++ Week4/Week4/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4
+{
+    internal class Car
+    {
+        public string color = "blue";
+        private int TC_id;
+        public int tc_id { 
+            get;
+
+            set;
+        }
+
+        private string name= "Enes";
+        public string Name {
+            get { return name; }
+            set { Name = value; }
+        }
+
+    }
+}
---
+++ Week4/Week4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4
+{
+    internal class Program
+    {
+        public string model;
+        string color = "red";
+        int maxSpeed = 200;
+        public void fullThrottle()
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+
+        public Program( string model, string color)
+        {
+            model = "Mustang";
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Program program = new Program("Mustang","red");
+            program.fullThrottle();
+            Console.WriteLine(program.color);
+
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+            myObj.tc_id = 1234;
+            Console.WriteLine(myObj.Name);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
